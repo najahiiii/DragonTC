2ebd47d68a3f770d04fc8f1ab4e8088d12cd951f
Najahiiii <najahiii@outlook.co.id>
2019-08-25 15:52:47 +0000

    build: Initial NusantaraTC
    Signed-off-by: Najahiiii <najahiii@outlook.co.id>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-git clone https://github.com/dragon-tc-tmp/build --depth=1 build
-git clone https://github.com/najahiiii/llvm-project --depth=1 llvm-project
+git clone https://github.com/najahiiii/Noob-Script.git -b build --depth=1 build
+git clone https://github.com/NusantaraDevs/llvm-project --depth=1 llvm-project
 git clone https://github.com/najahiiii/binutils -b 2.32 --depth=1 llvm-project/llvm/tools/binutils
 cd build || exit 1
 bash build_dtc 10.0
@@ -18,9 +18,8 @@ if ! [ -a lib64/libomp.so.5 ]; then
 fi
 chmod -R 777 $PREFIX_PATH
 git init
-git checkout -b daily/10.0
+git checkout -b 10.0
 git add .
-git commit -m "[daily]: $DTC_VERSION-DragonTC-$(date +'%d%m%y')" --signoff
-git remote add origin https://najahiiii:$GIT_TOKEN@github.com/NusantaraDevs/DragonTC.git
-git push --force origin daily/10.0
-
+git commit -m "$DTC_VERSION-NusantaraDevsTC-$(date +'%d%m%y')" --signoff
+git remote add origin https://najahiiii:$GIT_TOKEN@github.com/NusantaraDevs/clang.git
+git push --force origin 10.0
